8200824e06c5ece81c48a40cc6f48e1dda08fb59
Shivam Pant <shivam.pant.ext@proximus.com>
2026-03-05 11:47:48 +0530

    reverting to working code for function app

--- function_app.py
+++ function_app.py
@@ -3,26 +3,10 @@ from main import run_remediation
 
 app = func.FunctionApp()
 
-
-def find_build_id_in_payLoad(payload):
-    """ Extract build ID from Azure DevOps webhook payload."""
-    try:
-        resource = payload.get('resource', {})
-        build_id = resource.get('id')
-        if build_id is None:
-            raise ValueError("Build ID not found in payload")
-        return build_id
-    except Exception as e:
-        print(f"[FunctionApp ERROR] {str(e)}")
-        return None
-
 @app.route(route="webhook_trigger", methods=["POST"])
 def webhook_trigger(req: func.HttpRequest) -> func.HttpResponse:
     payload = req.get_json()
-    build_id = find_build_id_in_payLoad(payload)
-
-    if build_id is None:
-        return func.HttpResponse("Build ID not found", status_code=400)
+    build_id = payload['resource']['id']
 
     run_remediation(build_id)
 
